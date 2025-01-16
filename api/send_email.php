1c24240f7d4b14013d08d22a41c549a30c0bd654
Ashley <merienne.ashley@gmail.com>
2025-01-16 19:28:25 +0100

    refactor: update email sending function and improve email headers configuration

--- api/send_email.php
+++ api/send_email.php
@@ -1,16 +1,9 @@
 <?php
 require_once('../config/config.php');
 
-function send_email($to, $subject, $message) {
-    $headers = "From: " . EMAIL_FROM . "\r\n";
-    $headers .= "Reply-To: " . EMAIL_REPLY_TO . "\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-    if(mail($to, $subject, $message, $headers)) {
-        return true;
-    } else {
-        return false;
-    }
+function send_email(string $to, string $subject, string $message) {
+    $mailOK = mail($to, $subject, $message, HEADERS);
+    return $mailOK;
 }
 
 ?>
--- config/config.php
+++ config/config.php
@@ -5,12 +5,22 @@ define('ROOT_PATH', realpath(dirname(__FILE__)));
 
 // Database
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'your_database');
-define('DB_USER', 'your_username');
-define('DB_PASS', 'your_password');
+define('DB_NAME', 'prj-mmorich');
+define('DB_USER', 'prj-mmorich');
+define('DB_PASS', 'vZnmFpECUAvJTwCf');
 
 // Email
-define('EMAIL_FROM', 'your_email@example.com');
-define('EMAIL_REPLY_TO', 'your_email@example.com');
+define('EMAIL_FROM', 'prj-mmorich@projets.iut-orsay.fr');
+define('EMAIL_REPLY_TO', 'noreply@projets.iut-orsay.fr');
+define('HEADERS', [
+    'From' => 'prj-mmorich <' . EMAIL_FROM . '>',
+    'Cc' => 'prj-mmorich <' . EMAIL_FROM . '>',
+    'X-Sender' => 'prj-mmorich <' . EMAIL_FROM . '>',
+    'X-Mailer' => 'PHP/' . phpversion(),
+    'X-Priority' => '1',
+    'Return-Path' => EMAIL_REPLY_TO,
+    'MIME-Version' => '1.0',
+    'Content-Type' => 'text/html; charset=iso-8859-1'
+]);
 
 ?>
