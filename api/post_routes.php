5bd5f8a0e34e4a9efc2b8caabc5ce5813d48c05a
Ashley <merienne.ashley@gmail.com>
2025-01-19 17:58:07 +0100

    feat: implement POST route for adding propositions and enhance Proposition model with constructor

--- api/post_routes.php
+++ api/post_routes.php
@@ -1,1 +1,56 @@
+<?php
 
+/* This file is used to define routes for POST requests. */
+
+/*
+* Route: /test/post
+* This route is used to test POST requests.
+*/
+
+Router::addRoute('POST', '/proposition', function() {
+    post_proposition();
+});
+
+function post_proposition() {
+    require_once(ROOT_PATH . '/models/Proposition.php');
+
+    $data = json_decode(file_get_contents("php://input"));
+    $db = Database::$conn;
+
+    $titre = $data->titre;
+    $description = $data->description;
+
+    $proposition = new Proposition($titre, $description);
+
+    $query = "INSERT INTO Proposition::$table (titreProposition, descProposition)
+            VALUES (:name, :email, :proposition)";
+
+    $response = array(
+        "message" => "Proposition added successfully",
+        "data" => $data
+    );
+
+    echo json_encode($response);
+}
+
+/* This function is used to return a JSON response when fields are incomplete. */
+function fieldsIncomplete() {
+    $response = array(
+        "message" => "Please fill in all fields"
+    );
+
+    echo json_encode($response);
+}
+
+/*
+* This function is used to handle special characters in a string.
+* @param string $data The string to handle.
+* @return string The string with special characters handled.
+*/
+function handleChars(string $data) {
+    $data = htmlspecialchars($data);
+    $data = stripslashes($data);
+    return $data;
+}
+
+?>
--- models/Proposition.php
+++ models/Proposition.php
@@ -2,8 +2,26 @@
 require_once 'Modele.php';
 
 class Proposition extends Modele {
-    private static $cle = 'idProposition';
-    private static $table = 'Proposition';
+    protected static $cle = 'idProposition';
+    protected static $table = 'Proposition';
+
+    protected int $idProposition;
+    protected string $titre;
+    protected string $description;
+
+    /**
+     * Constructeur de la classe Proposition
+     * @param string $titre Le titre de la proposition
+     * @param string $description La description de la proposition
+    * @param int $idProposition L'identifiant de la proposition (optionnel)
+     */
+    public function __construct(string $titre, string $description, int $idProposition = -1) {
+        if ($idProposition !== -1) {
+            $this->idProposition = $idProposition;
+        }
+        $this->titre = $titre;
+        $this->description = $description;
+    }
 }
 
 ?>
