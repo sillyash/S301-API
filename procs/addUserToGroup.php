f527b8c708e032c758ec568890bef7dbaa0feb8b
Ashley <merienne.ashley@gmail.com>
2025-02-04 15:44:51 +0100

    feat: update input parameters in AddUserToGroup procedure to use 'login' and 'nomGrp'

--- procs/addUserToGroup.php
+++ procs/addUserToGroup.php
@@ -3,7 +3,7 @@ require_once 'Procedure.php';
 
 class AddUserToGroup extends Procedure {
     protected static string $name = 'addUserToGroup';
-    protected static ?array $in = ['id_user', 'id_group'];
+    protected static ?array $in = ['login', 'nomGrp'];
     protected static ?array $out = null;
 }
 
